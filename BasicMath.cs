46dd473c170093fb2a6547f0a13aede932e79383
Jamie <espiritu_jamie@hotmail.com>
2023-03-24 12:47:45 -0600

    Add project files.

---
+++ BasicMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath
+{
+    public class BasicMath
+    {
+        public double Add(double a, double b)
+        {
+            double value = a + b;
+            return value;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double value = a - b;
+            return value;
+        }
+
+        public double Divide(double a, double b)
+        {
+            double value = a / b;
+            return value;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double value = a * b;
+            return value;
+        }
+    }
+}
